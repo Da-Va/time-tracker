e49f58828cbfda987bf8aae094cf359cba518fe2
Dave <david1valouch@gmail.com>
2023-08-24 01:33:00 +0200

    Add config parsing

--- main.py
+++ main.py
@@ -2,23 +2,33 @@
 
 import sys
 import subprocess
+import json
 
 def main(argv):
+    config_file = 'example_config.json'
+    with open(config_file, 'r') as config_f:
+        config = json.load(config_f)
+
     smenu_command = 'smenu'
 
     smenu_args = [
         smenu_command,
-        '-r' #Enable Confirmation in seach mode
+        '-m', 'Select activity:',
+        '-r', #Enable Confirmation in seach mode.
+        '-t', '1', #Set single collumn.
+        '-W', '\n',
+        '-N',
+        '-D', 'd:_',
     ]
 
-    tasks = "one two three four five"
+    tasks = [ t['name'] + 5*' ' + ','.join(t['tags']) for t in config['activities'] ]
+    tasks = '\n'.join(tasks)
 
-    selection = subprocess.check_output(smenu_args, input=tasks.encode()) 
-    selection = selection.decode()
+    selection = subprocess.check_output(smenu_args, input=tasks, text=True)
 
-    print("Selection: ", selection)
+    print(f"Selection: '{selection}'")
 
 
-    
+
 if __name__=='__main__':
     main(sys.argv)
